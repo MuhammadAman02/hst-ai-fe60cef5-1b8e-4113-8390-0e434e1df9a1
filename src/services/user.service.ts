ecdba2931688a9ec48710bbf84fd63b9b050890c
MuhammadAman02 <mohammada@hst.ie>
2025-06-11 19:02:57 +0500

    update file

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,18 +1,31 @@
-import { PrismaClient } from "@prisma/client";
-import bcrypt from "bcrypt";
+import { PrismaClient, Prisma } from '@prisma/client';
+import { AppError } from '../utils/AppError';
+import bcrypt from 'bcrypt';
 
 const prisma = new PrismaClient();
 
-export async function createUser({
-  email,
-  password,
-}: {
-  email: string;
-  password: string;
-}) {
-  const hashed = await bcrypt.hash(password, 10);
-  return prisma.user.create({
-    data: { email, password: hashed },
-    select: { id: true, email: true },
+export async function createUser({ email, password }: { email: string; password: string }) {
+  const hashedPassword = await bcrypt.hash(password, 10);
+  
+  try {
+    return await prisma.user.create({
+      data: { email, password: hashedPassword },
+      select: { id: true, email: true },
+    });
+  } catch (error: any) {
+    if (error instanceof Prisma.PrismaClientKnownRequestError && error.code === 'P2002') {
+      throw new AppError('Email already exists', 409);
+    }
+    throw new AppError('Failed to create user');
+  }
+}
+
+export async function getUsers() {
+  return await prisma.user.findMany({
+    select: {
+      id: true,
+      email: true,
+      createdAt: true,
+    },
   });
 }
